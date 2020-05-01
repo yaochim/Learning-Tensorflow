d1b455e5424f1dba5e2db00e569cda063df66328
Yao on Flex-Ryxen-MXnix <yaochim8@gmail.com>
2020-05-01 19:29:22 +0100

    tensorflow imports

--- tutorial1.py
+++ tutorial1.py
@@ -1,6 +1,14 @@
 import tensorflow as tf
 from tensorflow import keras
-import numpyas as np
+import numpy as np
 import matplotlib.pyplot as plt
 
 data = keras.datasets.fashion_mnist
+
+(train_images, train_labels), (test_images, test_labels) = data.load_data()
+
+class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat', 'Sandal',
+               'Shirt', 'Sneaker', 'Bag', 'Ankle Boot']
+
+plt.imshow(train_images[7], cmap=plt.cm.binary)
+plt.savefig("img.png")
